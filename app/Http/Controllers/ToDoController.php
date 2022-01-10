82267005fce5087fdd0cbfb0b14597d7f7e7ec50
船橋渓 <funahashikei@team-lab.com>
2022-01-10 18:16:28 +0900

    add: 新規作成機能を実装

--- app/Http/Controllers/ToDoController.php
+++ app/Http/Controllers/ToDoController.php
@@ -7,11 +7,31 @@ use App\Models\Todo;
 
 class ToDoController extends Controller
 {
+    // 一覧画面を表示
     public function index()
     {
         $todos = Todo::orderBy('id', 'asc')->get();
         return view('list', [
             "todos" => $todos
         ]);
+    }
+
+    // タスク作成画面を表示
+    public function createPage()
+    {
+        return view('todo_create');
+    }
+
+    // タスクを登録
+    public function create(Request $request)
+    {
+        $todo = new Todo();
+        $todo->task_name = $request->task_name;
+        $todo->task_description = $request->task_description;
+        $todo->assign_person_name = $request->assign_person_name;
+        $todo->estimate_hour = $request->estimate_hour;
+        $todo->save();
+
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\ToDoController@index');
+Route::get('/create-page', 'App\Http\Controllers\ToDoController@createPage');
+Route::post('/create', 'App\Http\Controllers\ToDoController@create');
